59b73608173e48c97e62b79254139db98f0293e9
jordans <schuringa.jordan@gmail.com>
2017-07-21 12:28:09 +1000

    Added actions for email changed.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { Card, CardSection, Input, Button } from './common'
 
 class LoginForm extends Component {
+  onEmailChange (text) {}
   render () {
     return (
       <Card>
         <CardSection>
-          <Input label='Email' placeholder='email@gmail.com' />
+          <Input
+            label='Email'
+            placeholder='email@gmail.com'
+            onChangeText={this.onEmailChange.bind(this)}
+          />
         </CardSection>
 
         <CardSection>
