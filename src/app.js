14956b1b9428ef8a578bfb3e1f4bcb295bbb1cb7
jordans <schuringa.jordan@gmail.com>
2017-07-21 10:56:17 +1000

    Added firebase config

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
 
 class App extends Component {
+  componentWillMount () {
+    const config = {
+      apiKey: 'AIzaSyAABNmZC7DKRovf8Yxcg13dl6lOqjKizr0',
+      authDomain: 'employee-manager-678de.firebaseapp.com',
+      databaseURL: 'https://employee-manager-678de.firebaseio.com',
+      projectId: 'employee-manager-678de',
+      storageBucket: '',
+      messagingSenderId: '736860961405'
+    }
+    firebase.initializeApp(config)
+  }
+
   render () {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello!</Text>
         </View>
