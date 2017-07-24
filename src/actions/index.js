5075ee34122bb1d075e31e2b3398388fce13183b
jordans <schuringa.jordan@gmail.com>
2017-07-24 13:04:27 +1000

    Added helper methods for login user success and login user fail. Added new type for login user fail.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase'
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types'
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from './types'
 
 export const emailChanged = text => {
   return {
@@ -17,8 +22,27 @@ export const passwordChanged = text => {
 
 export const loginUser = ({ email, password }) => {
   return dispatch => {
-    firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
-      dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
-    })
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFailed)
+      })
   }
+}
+
+const loginUserFailed = dispatch => {
+  dispatch({ type: LOGIN_USER_FAIL })
+}
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  })
 }
