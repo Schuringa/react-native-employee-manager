ed49329bba5254ba27a726e2f05812d071402d4f
jordans <schuringa.jordan@gmail.com>
2017-07-24 13:52:35 +1000

    Added console log for catching firebase errors

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -13,15 +13,13 @@ const INITIAL_STATE = {
 }
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action)
-
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload }
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload }
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload }
+      return { ...state, user: action.payload, error: '' }
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication failed.', password: '' }
     default:
