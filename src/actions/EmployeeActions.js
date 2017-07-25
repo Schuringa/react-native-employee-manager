2f9cdac528438e7860556c5e57a2d76a83c7fa0f
jordans <schuringa.jordan@gmail.com>
2017-07-25 10:39:25 +1000

    Moved auth acitons into its own file.  Created Employee actions file. Create employee form reducer. Added input and button fields into the employee create form

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,9 @@
+// import Actions from 'react-native-router-flux'
+import { EMPLOYEE_UPDATE } from './types'
+
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  }
+}
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
   render () {
     return (
-      <View>
-        <Text> EMployee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label='Name' placeholder='Jane' />
+        </CardSection>
+        <CardSection>
+          <Input label='Phone' placeholder='99999999' />
+        </CardSection>
+        <CardSection>
+          <Button>Create</Button>
+        </CardSection>
+      </Card>
     )
   }
 }
